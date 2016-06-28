6228e2509c8b1fa4f83b658c10b5a05b3eb63217
Kevin Campuzano <kevincamp_90@me.com>
2016-06-28 17:03:39 -0500

    Example of ThreadPool

--- threadSimpleExample.py
+++ threadSimpleExample.py
@@ -1,5 +1,25 @@
 import time
 import threading
+from multiprocessing.dummy import Pool as ThreadPool
+
+def squareNumber(n):
+	return n**2
+
+def calculateParallel(numbers, threads=2):
+	pool = ThreadPool(threads)
+	results = pool.map(squareNumber, numbers)
+	pool.close()
+	pool.join()
+	return results
+
+
+if __name__ == "__main__":
+	numbers = [1,2,3,4,5]
+	squaredNumbers = calculateParallel(numbers, 4)
+
+	for n in squaredNumbers:
+		print(n)
+
 
 clients_list = ()
 threads = list()
@@ -9,13 +29,13 @@ def imprimir_mensaje(mensaje):
 		print(mensaje)
 		time.sleep(1)
 
-def main():
-	mensaje = "Thread 1" #Variable aux
-	mensaje2 = "Thread 2" #Variable aux
-	#Empiezo el thread
-	thread.start_new_thread(imprimir_mensaje, (mensaje,))
-	thread.start_new_thread(imprimir_mensaje,(mensaje2,))
-	x = raw_input("Estoy esperando que presione Enter...")
-	print("Termino la funcion main")
+# def main():
+# 	mensaje = "Thread 1" #Variable aux
+# 	mensaje2 = "Thread 2" #Variable aux
+# 	#Empiezo el thread
+# 	thread.start_new_thread(imprimir_mensaje, (mensaje,))
+# 	thread.start_new_thread(imprimir_mensaje,(mensaje2,))
+# 	x = raw_input("Estoy esperando que presione Enter...")
+# 	print("Termino la funcion main")
 
-main()
+# main()
