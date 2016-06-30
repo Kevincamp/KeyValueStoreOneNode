671bc9df48b2c15766913d358b8ee49017255e00
Kevin Campuzano <kevincamp_90@me.com>
2016-06-29 23:41:48 -0500

    MainThread class waiting for a command

--- mainThread.py
+++ mainThread.py
@@ -1,14 +1,20 @@
 import threading
+from time import sleep
 
 class MainThread(threading.Thread):
-	def __init__(self, num):
+	def __init__(self, command):
 		threading.Thread.__init__(self)
-		self.num = num
-	def run(self):
-		print "Soy el hilo", self.num
+		self.command = command
 
-for i in range(0,10):
-	t = MainThread(i)
+	def run(self):
+		if self.command != "":
+			print "El comando es: " + self.command
+		else:
+			sleep(5)
+		
+comandos = ["del -a","add b", ""]
+for comando in comandos:
+	t = MainThread(comando)
 	t.start()
 	t.join()
-	print "Fin."
+	print "Fin"
