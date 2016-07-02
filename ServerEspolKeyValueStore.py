592e8512203e1cb1084c1ca01829f39db2c68449
Erika Narvaez <enarvaez@espol.edu.ec>
2016-07-01 22:21:24 -0500

    Sencilla prueba

--- ServerEspolKeyValueStore.py
+++ ServerEspolKeyValueStore.py
@@ -7,14 +7,16 @@ from mainThread import MainThread
 def loadWork(spool,socketclt):  #Verifica cada hilo, si el hilo que esta escuchando no tiene socket y lo pone a trabajar(se lo pasa)
     for index,the_thread in enumerate(spool):
         if the_thread.getClientSocket() is None:
-            the_thread.setClientSocket(socketclt)
+            the_thread.setClientSocket(socketclt)dal
             return True
     return False
 
 # inicializa los hilos y los mete un pool(lista)
 def initThreads(pool,threadNumber):
     for i in range(threadNumber):
-
+        thread = MainThread()
+        thread.start()
+        pool.append(thread)
 
 s = socket.socket()               #Se crea el objeto socket
 host = socket.gethostname()     #Se obtiene el hostname de la máquina
