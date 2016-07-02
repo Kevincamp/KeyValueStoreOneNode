38624523dc03f703e773d32c6d9ce6856f649628
Erika Narvaez <enarvaez@bitbucket.org>
2016-07-01 22:17:33 -0500

    Prueba 2

--- ServerEspolKeyValueStore.py
+++ ServerEspolKeyValueStore.py
@@ -14,9 +14,7 @@ def loadWork(spool,socketclt):  #Verifica cada hilo, si el hilo que esta escucha
 # inicializa los hilos y los mete un pool(lista)
 def initThreads(pool,threadNumber):
     for i in range(threadNumber):
-        thread = MainThread()
-        thread.start()
-        pool.append(thread)
+
 
 s = socket.socket()               #Se crea el objeto socket
 host = socket.gethostname()     #Se obtiene el hostname de la máquina
