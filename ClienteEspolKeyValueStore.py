b1be5e41a6872b627a7d365d5d37da6a690ee25f
Dimitri Laaz <dimitri.laaz.1990@gmail.com>
2016-07-03 01:07:47 -0500

    añadido candados, correccion list, verificacion de conexion activa en el despachador

--- ClienteEspolKeyValueStore.py
+++ ClienteEspolKeyValueStore.py
@@ -7,62 +7,60 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def main():
-    if len(sys.argv) != 3:
-        print "ERROR: faltan o sobran argumentos; ej: <ip del servidor> <puerto>"
-        sys.exit(0);
-    host = sys.argv[1]
-    port = int(sys.argv[2])  #12345
-             # Create a socket object
-    exflag = True
-    s.connect((host, port))
-    while exflag:
-        exflag = requestInput();
-    sys.exit(0)
+	if len(sys.argv) != 3:
+		print "ERROR: faltan o sobran argumentos; ej: <ip del servidor> <puerto>"
+		sys.exit(0);
+	host = sys.argv[1]
+	port = int(sys.argv[2])  #12345
+	exflag = True
+	s.connect((host, port))
+	print "Conectando..."
+	if s.recv(8) == "ok":
+		while exflag:
+			exflag = requestInput();
+	sys.exit(0)
 
 def requestInput():
-    entry = str(raw_input(">> "))
-    #regex = "^([a-zA-Z0-9ñÑ_\-\/\\\.@áéíóú'!¡¿?+*:;]+)[ \t\r\f\v]+(\S+)[ \t\r\f\v]+([ \t\r\f\va-zA-Z0-9ñÑ_\-\/\\\.@áéíóú'!¡¿?+*:;]+)$"
-    regex = "^(\S+)[ \t\r]*(\S*)[ \t\r]*([ \t\r\S]*)$"
-    patron = re.compile(regex)
-    m = patron.match(entry)
-    if m is not None:
-        instruccion = str(m.group(1)).lower()
-        clave = str(m.group(2))
-        valor = str(m.group(3))
-        #inslen = len(m.groups())
-        if instruccion == "exit" and clave == "" and valor == "":
-            s.close()
-            print "Cerrando la conexión..."
-            return False
-        elif instruccion == "help" and clave == "" and valor == "":
-            print "get key: Operación ​get. Retorna el valor asociado a dicha clave"
-            print "set key value: Almacena (en memoria) la clave, con el valor asociado."
-            print "del key: Elimina la clave, con su valor asociado."
-            print "list: Retorna la lista de todas las claves almacenadas"
-            print "exit: Cierra la conexión con el servidor y termina la ejecución del programa"
-            print "help: Muestra la lista de los comandos soportados, incluyendo una breve explicación de los mismos"
-        elif instruccion == "get" and clave != "" and valor == "":
-            s.send(instruccion+' '+clave+' '+' ')
-            print s.recv(4096)
-            pass
-        elif instruccion == "del" and clave != "" and valor == "":
-            s.send(instruccion+' '+clave+' '+valor)
-            print s.recv(1024)
-            pass
-        elif instruccion == "set" and clave != "" and valor != "":
-            s.send(instruccion+' '+clave+' '+valor)
-            print s.recv(4096)
-            pass
-        elif instruccion == "list" and clave == "" and valor == "":
-            s.send(instruccion+ ' ' + ' ')
-            #print s.recv(1024)
-            pass
-        else:
-            print "ERROR: la instrucción dada no es válida"
+	entry = str(raw_input(">> "))
+	regex = "^(\S+)[ \t\r]*(\S*)[ \t\r]*([ \t\r\S]*)$"
+	patron = re.compile(regex)
+	m = patron.match(entry)
+	if m is not None:
+		instruccion = str(m.group(1)).lower()
+		clave = str(m.group(2))
+		valor = str(m.group(3))
+		#inslen = len(m.groups())
+		if instruccion == "exit" and clave == "" and valor == "":
+			s.send("exit")
+			s.close()
+			print "Cerrando la conexión..."
+			return False
+		elif instruccion == "help" and clave == "" and valor == "":
+			print "get key: Operación ​get. Retorna el valor asociado a dicha clave"
+			print "set key value: Almacena (en memoria) la clave, con el valor asociado."
+			print "del key: Elimina la clave, con su valor asociado."
+			print "list: Retorna la lista de todas las claves almacenadas"
+			print "exit: Cierra la conexión con el servidor y termina la ejecución del programa"
+			print "help: Muestra la lista de los comandos soportados, incluyendo una breve explicación de los mismos"
+		elif instruccion == "get" and clave != "" and valor == "":
+			#s.send(instruccion+' '+clave+' '+' ')
+			s.send(str(entry))
+			print s.recv(4096)
+		elif instruccion == "del" and clave != "" and valor == "":
+			s.send(instruccion+' '+clave+' '+valor)
+			print s.recv(1024)
+		elif instruccion == "set" and clave != "" and valor != "":
+			s.send(instruccion+' '+clave+' '+valor)
+			print s.recv(4096)
+		elif instruccion == "list" and clave == "" and valor == "":
+			s.send(entry)
+			print s.recv(4096)
+		else:
+			print "ERROR: la instrucción dada no es válida"
 
-    else:
-        print "ERROR: la instrucción dada no es válida"
-    return True
+	else:
+		print "ERROR: la instrucción dada no es válida"
+	return True
 
 if __name__ == "__main__":
-    main()
+	main()
--- mainThread.py
+++ mainThread.py
@@ -1,81 +1,101 @@
+# coding=utf-8
 import threading
+from threading import Lock
 from time import sleep
 import socket
-
+import re
 class MainThread(threading.Thread):
-    cache = {} # variable de clase
+	cache = {} # variable de clase
+	candado = Lock()
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.clientSocket = None #variable de instancia
 
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.clientSocket = None #variable de instancia
+	def run(self):
+		while True:
+			if self.clientSocket is not None:
+				self.comando(self.clientSocket)
+			else:
+				sleep(1)
 
-    def run(self):
-        while True:
-            if self.clientSocket is not None:
-                print self.clientSocket.getpeername() #codigo para probar
-                self.comando(self.clientSocket)
-                sleep(60)
-            else:
-                #print "entro"
-                sleep(1)
+	def setClientSocket(self,s):
+		self.clientSocket = s
+		self.clientSocket.settimeout(5)
+		try:
+			self.clientSocket.send("ok")
+			self.clientSocket.settimeout(None)
+		except socket.timeout:
+			self.clientSocket.close()
+			self.clientSocket = None	
+		
 
-    def setClientSocket(self,s):
-        self.clientSocket = s
+	def getClientSocket(self):
+		return self.clientSocket
 
-    def getClientSocket(self):
-        return self.clientSocket
+	#Funcion que ejecuta el comando "set"
+	def setDict(self,key,value): #SET
+		self.candado.acquire()
+		if key in self.cache:
+			self.cache[key] = value
+		else:
+			self.cache[key] = value
+		res = 'OK'
+		self.candado.release()
+		return res
 
-    #Funcion que ejecuta el comando "set"
-    def setDict(self,key,value):
-        if key in self.cache:
-            self.cache[key] = value
-        else:
-            self.cache[key] = value
-        res = 'OK'
-        return res
-
-    #Funcion que ejecuta el comando "del"
-    def delDict(self,key):
-        if key in self.cache:
-            del self.cache[key]
-            res='Elemento eliminado'
-        else:
-            res='ERROR: No existe un valor asociado a esa clave en el diccionario'
-        return res
+	#Funcion que ejecuta el comando "del"
+	def delDict(self,key): #DEL
+		self.candado.acquire()
+		if key in self.cache:
+			del self.cache[key]
+			res='Elemento eliminado'
+		else:
+			res='ERROR: No existe un valor asociado a esa clave en el diccionario'
+		self.candado.release()
+		return res
 
 
-    def getDictValue(self,key):
-      res = ""
-      for self.key in self.cache.keys():
-        if key == self.key:
-            res = self.cache[self.key]
-        break
-      return res
+	def getDictValue(self,key): #GET
+		res = "key = "
+		self.candado.acquire()
+		for clave in self.cache.keys():
+			if key == clave:
+				res = res + str(self.cache[clave])
+				break
+		self.candado.release()
+		return res
 
-    def getDict(self):
-        res = ""
-        for self.key in self.cache.keys():
-            res += self.key +"->"+self.cache[self.key]+"\n"
-        return res
+	def getDict(self): #LIST
+		res = ""
+		self.candado.acquire()
+		for clave in self.cache.keys():
+			res += clave + "\n"
+		self.candado.release()
+		return res + " "
 
-    def comando(self,s):
-        com = s.recv(4096)
-        tempComando = com.split(' ')
-        cmd=tempComando[0]
-        key = tempComando[1]
-        value = tempComando[2]
+	def comando(self,s):
+		com = s.recv(4096)
+		regex = "^(\S+)[ \t\r]*(\S*)[ \t\r]*([ \t\r\S]*)$"
+		patron = re.compile(regex)
+		tempComando = patron.match(com)
+		cmd = str(tempComando.group(1)).lower()
+		key = str(tempComando.group(2))
+		value = str(tempComando.group(3))
 
-        if cmd == 'set':
-            res = self.setDict(key,value)
-            s.send(res)
-        elif cmd == 'get':
-            res = self.getDictValue(key)
-            s.send(res)
-<<<<<<< HEAD
-        elif cmd == 'list':
-            res = self.getDict()
-=======
-        elif cmd=='del':
-            res=self.delDict(key)
->>>>>>> 8917f9aaa0091a783579a8750e7e1db41a9b3a2a
-            s.send(res)
+		if cmd == 'set':
+			res = self.setDict(key,value)
+			s.send(res)
+		elif cmd == 'get':
+			res = self.getDictValue(key)
+			s.send(res)
+		elif cmd == 'list':
+			res = self.getDict()
+			s.send(res)
+		elif cmd =='del':
+			res=self.delDict(key)
+			s.send(res)
+		elif cmd =='exit':
+			self.clientSocket.close()
+			self.clientSocket = None
+		else:
+			s.send('ERROR: Instrucción no reconocida por el servidor')
