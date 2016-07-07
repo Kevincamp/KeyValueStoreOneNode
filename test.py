4efcb7c6ac4c4d262b1aa837a550cc3ad7354a48
KevinCamp <kevincamp_90@me.com>
2016-07-07 11:38:47 -0500

    how to run the project - pictures

--- test.py
+++ test.py
@@ -2,11 +2,18 @@ import unittest
 from mainThread import MainThread
 
 class TestCase(unittest.TestCase):
- 
+ 	cache = {[a:b]} # variable de clase
     def test_set(self):
     	thread = MainThread()
         setDict = thread.setDict('a','b')
         self.assertEquals("OK", setDict)
+    def test_set2(self):
+    	thread = MainThread()
+    	setDict = thread.setDict('b','')
+    	self.assertEquals("OK", setDict)
+    def test_thread(self):
+    	thread = create_autospec(MainThread)
+    	
 
 if __name__ == '__main__':
         unittest.main()
