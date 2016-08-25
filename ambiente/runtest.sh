a5337f7a95ae5c8b917638ca7941fb80cc990cc4
Dimitri laaz <dimitri.laaz.1990@gmail.com>
2016-08-24 21:35:08 -0500

    añadidas pruebas de lectura y escritura base nuestra

--- ambiente/runtest.sh
+++ ambiente/runtest.sh
@@ -7,15 +7,15 @@ workload="read.txt"
 db="espol"
 host="espol.host=127.0.0.1"
 port="espol.port=12345"
-for i in 1 2 3 4 5
+for i in 8 10
 do
 	for j in 1 2 3 4 5
 	do
 		pkill python
 		python ./propio/ServerEspolKeyValueStore.py &
-		./bin/ycsb load $db -s -P workloads/$workload -p "$host" -p "$port" -threads $i > ./pruebas/$operacion$i$j.txt
-		./bin/ycsb run $db -s -P workloads/$workload -p "$host" -p "$port" -threads $i> ./pruebas/$operacion$i$j.txt
+		./bin/ycsb load $db -s -P workloads/$workload -p "$host" -p "$port" -threads $i > ./pruebas/load$operacion$i$j.txt
+		./bin/ycsb run $db -s -P workloads/$workload -p "$host" -p "$port" -threads $i> ./pruebas/run$operacion$i$j.txt
 		pkill python
-		echo "Terminado test hilo: " $i "iteracion: " $j
+		echo "++++++++++EJECUCION TERMINADA EN HILO: " $i "ITERACION: " $j" ++++++++++++++"
 	done
 done
